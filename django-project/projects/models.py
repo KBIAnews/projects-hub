d8e405ec85e8952f85524297a258ea2424b5027c
Nathan Lawrence <nathan.Lawrence.barrick@gmail.com>
2018-04-06 14:35:23 -0500

    Project model, fixed gitignore

--- django-project/projects/models.py
+++ django-project/projects/models.py
@@ -4,3 +4,19 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Project (models.Model):
+    slug = models.SlugField(max_length=128,
+                            unique=True,
+                            blank=False,
+                            null=False)
+
+    name = models.TextField(max_length=512,
+                            blank=False,
+                            null=False)
+
+    def get_absolute_url(self):
+        return "/%s/" % self.slug
+
+    class Meta:
+        ordering = ['slug']
